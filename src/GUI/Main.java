09370e8fa197b1882bacb6a139714d4f7642738f
micdra01 <114472076+micdra01@users.noreply.github.com>
2022-11-28 15:58:24 +0100

    Improved FXML view and a main method that launches it.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,7 +1,28 @@
 package GUI;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Views/MainView.fxml"));
+        primaryStage.setTitle("MyTunes | Gruppe 2");
+        primaryStage.setScene(new Scene(root));
+
+        //Fixed width & height. Could be removed once HGrow & VGrow has been set correctly.
+        primaryStage.setMinWidth(1010.0);
+        primaryStage.setMinHeight(640.0);
+        primaryStage.setMaxWidth(1010.0);
+        primaryStage.setMaxHeight(640.0);
+
+        primaryStage.show();
     }
 }
