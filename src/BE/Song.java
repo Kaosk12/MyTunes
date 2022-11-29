786f4b70ee5b6c6dc5c9c8a1eb1cb807eb00b4a1
JennyTromTheBlock <kenmad01@easv365.dk>
2022-11-29 11:00:58 +0100

    DAL interfaces and Song entity.

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -1,30 +1,18 @@
 package BE;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-
 public class Song {
 
     private String title;
     private String artist;
     private String genre;
     private int time;
-    /*private SimpleStringProperty title;
-    private SimpleStringProperty authour;
-
-    private SimpleStringProperty genre;
-
-    private SimpleIntegerProperty time;*/
 
     public Song(String title, String artist, String genre, int time){
         this.title = title;
         this.artist = artist;
         this.genre = genre;
         this.time = time;
-        /*this.title = new SimpleStringProperty(Title);
-        this.authour = new SimpleStringProperty(Authour);
-        this.genre = new SimpleStringProperty(genre);
-        this.time = new SimpleIntegerProperty(time);*/
+
     }
 
 
